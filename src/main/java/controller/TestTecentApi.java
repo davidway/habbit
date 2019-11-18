2093cc2424f5d0a110dae9a3cf659923765bd2b2
zhuweiqiang <zhuweiqiang@mail.weisanyun.com>
2019-11-18 14:14:47 +0800

    tbaas 初始化

---
+++ src/main/java/controller/TestTecentApi.java
@@ -0,0 +1,34 @@
+package controller;
+
+import com.tencentcloudapi.common.Credential;
+import com.tencentcloudapi.common.profile.ClientProfile;
+import com.tencentcloudapi.common.profile.HttpProfile;
+import com.tencentcloudapi.common.exception.TencentCloudSDKException;
+
+import com.tencentcloudapi.tbaas.v20180416.TbaasClient;
+
+import com.tencentcloudapi.tbaas.v20180416.models.ApplyUserCertRequest;
+import com.tencentcloudapi.tbaas.v20180416.models.ApplyUserCertResponse;
+
+
+public class TestTecentApi {
+
+
+    public static void testApplyAccount() {
+        try {
+            try {
+                Credential cred = new Credential("AKIDEdxIkFJS2SOHFERuW0JUjlcIm4GGHg2i", "udDbqvpkvGVZTNG2byFLFw9FNg1DE5kK");
+                HttpProfile httpProfile = new HttpProfile();
+                httpProfile.setEndpoint("tbaas.tencentcloudapi.com");
+                ClientProfile clientProfile = new ClientProfile();
+                clientProfile.setHttpProfile(httpProfile);
+                TbaasClient client = new TbaasClient(cred, "ap-guangzhou", clientProfile);
+                String params = "{\"Module\":\"cert_mng\",\"Operation\":\"cert_apply_for_user\",\"ClusterId\":\"ch3a5nqj860reumytw\",\"GroupName\":\"a\",\"UserIdentity\":\"a\",\"Applicant\":\"a\",\"IdentityNum\":\"a\",\"CsrData\":\"a\"}";
+                ApplyUserCertRequest req = ApplyUserCertRequest.fromJsonString(params, ApplyUserCertRequest.class);
+                ApplyUserCertResponse resp = client.ApplyUserCert(req);
+            } catch (TencentCloudSDKException e) {
+                System.out.println(e.toString());
+            }
+
+        }
+    }
