5a0782000afe0154e48b6b42c3d13056d39ca06c
zhuweiqiang <zhuweiqiang@mail.weisanyun.com>
2019-11-25 15:51:28 +0800

    测试

--- src/test/java/TestApi.java
+++ src/test/java/TestApi.java
@@ -35,7 +35,7 @@ public class TestApi {
                 + executor.getActiveCount());
         System.out.println("Total number of threads(ever scheduled): "
                 + executor.getTaskCount());
-
+        System.out.println();
         executor.shutdown();
     }
 
